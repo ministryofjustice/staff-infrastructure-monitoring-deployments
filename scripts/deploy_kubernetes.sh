ad17f2d6ba7dc533edd9a490d42fe533bbaaca19
Touhidul Islam <touhidul.islam@justice.gov.uk>
2021-04-22 15:38:02 +0100

    script fixup

--- scripts/deploy_kubernetes.sh
+++ scripts/deploy_kubernetes.sh
@@ -10,7 +10,7 @@ echo $terraform_outputs
 export KUBECONFIG="./kubernetes/kubeconfig"
 # outputs=$(cat ./terraform_outputs.json)
 env=$(echo $TERRAFORM_OUTPUTS | jq '.env.value' | sed 's/"//g')
-assume_role=$(echo $OUTPUTS | jq '.assume_role.value' | sed 's/"//g')
+assume_role=$(echo $TERRAFORM_OUTPUTS | jq '.assume_role.value' | sed 's/"//g')
 # echo $assume_role
 TEMP_ROLE=`aws sts assume-role --role-arn $assume_role --role-session-name ci-authenticate-kubernetes-782`
 
@@ -18,11 +18,11 @@ access_key=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.AccessKeyId')
 secret_access_key=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SecretAccessKey')
 session_token=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SessionToken')
 
-cluster_role_arn=$(echo $OUTPUTS | jq '.eks_cluster_worker_iam_role_arn.value' | sed 's/"//g')
-prometheus_image_repo=$(echo $OUTPUTS | jq '.prometheus_repository_v2.value.repository_url' | sed 's/"//g')
-cluster_name=$(echo $OUTPUTS | jq  '.eks_cluster_id.value' | sed 's/"//g')
-prometheus_thanos_storage_bucket_name=$(echo $OUTPUTS | jq '.prometheus_thanos_storage_bucket_name.value' | sed 's/"//g')
-prometheus_thanos_storage_kms_key_id=$(echo $OUTPUTS | jq '.prometheus_thanos_storage_kms_key_id.value' | sed 's/"//g')
+cluster_role_arn=$(echo $TERRAFORM_OUTPUTS | jq '.eks_cluster_worker_iam_role_arn.value' | sed 's/"//g')
+prometheus_image_repo=$(echo $TERRAFORM_OUTPUTS | jq '.prometheus_repository_v2.value.repository_url' | sed 's/"//g')
+cluster_name=$(echo $TERRAFORM_OUTPUTS | jq  '.eks_cluster_id.value' | sed 's/"//g')
+prometheus_thanos_storage_bucket_name=$(echo $TERRAFORM_OUTPUTS | jq '.prometheus_thanos_storage_bucket_name.value' | sed 's/"//g')
+prometheus_thanos_storage_kms_key_id=$(echo $TERRAFORM_OUTPUTS | jq '.prometheus_thanos_storage_kms_key_id.value' | sed 's/"//g')
 
 # SAVE KUBECONFIG FILE
 AWS_ACCESS_KEY_ID=$access_key AWS_SECRET_ACCESS_KEY=$secret_access_key AWS_SESSION_TOKEN=$session_token aws eks\
